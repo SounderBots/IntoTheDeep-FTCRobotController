8700ed64fa8489cd2a9246dc0761d559d4322bb8
Tony H <tony.han.lele@gmail.com>
2025-05-09 18:13:15 -0700

    Intake slider test code, will be added to invitation bot branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeSlider.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.subsystems.intake;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.SonicSubsystemBase;
+import org.firstinspires.ftc.teamcode.subsystems.feedback.DriverFeedback;
+
+
+public class IntakeSlider extends SonicSubsystemBase {
+
+    private CRServo servo1;
+
+    private CRServo servo2;
+
+    private Telemetry telemetry;
+
+    public IntakeSlider(HardwareMap hardwareMap, Telemetry telemetry) {
+        /* instantiate motors */
+        this.servo1  = hardwareMap.get(CRServo.class,"IntakeServo1");
+        this.servo2  = hardwareMap.get(CRServo.class,"IntakeServo2");
+
+        this.telemetry = telemetry;
+    }
+
+    boolean isExpanding = false;
+
+    public void ToggleExpand() {
+        if(isExpanding) {
+            Hold();
+        } else {
+            Expand();
+        }
+
+        isExpanding = !isExpanding;
+    }
+
+    boolean isCollapsing = false;
+
+    public void ToggleCollapse() {
+        if(isCollapsing) {
+            Hold();
+        } else {
+            Collapse();
+        }
+
+        isCollapsing = !isCollapsing;
+    }
+
+    public void Expand() {
+        this.servo1.setPower(-1);
+        this.servo2.setPower(1);
+    }
+
+    public void Collapse() {
+        this.servo1.setPower(1);
+        this.servo2.setPower(-1);
+    }
+
+    public void Hold() {
+        this.servo1.setPower(0.0);
+        this.servo2.setPower(0.0);
+    }
+
+    public void AutoExpand() {
+        this.servo1.setPower(-1);
+        this.servo2.setPower(1);
+
+        this.servo1.setPower(0);
+        this.servo2.setPower(0);
+    }
+}
